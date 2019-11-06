b9d45f4838266d4a66493386904123c16817436f
Fabien Boucher <fboucher@redhat.com>
2019-11-06 11:50:49 +0100

    Support python3 remove python2 support
    Depends-on: https://softwarefactory-project.io/r/16960
    
    Change-Id: I61814a9f69ee9adcffd95ff2a5573a47619033ef

--- cauth/controllers/root.py
+++ cauth/controllers/root.py
@@ -16,7 +16,7 @@
 # under the License.
 
 import logging
-from urllib import unquote
+from urllib.parse import unquote
 
 from pecan import expose, request, response, conf
 from pecan.rest import RestController
@@ -56,23 +56,23 @@ class RootController(object):
         login.github = github.GithubController()
         login.githubAPIkey = github.PersonalAccessTokenGithubController()
     except exceptions.AuthProtocolNotAvailableError as e:
-        logger.debug("%s - skipping callback endpoint" % e.message)
+        logger.debug("%s - skipping callback endpoint" % e)
     try:
         login.oauth2 = oauth2.OAuth2Controller()
     except exceptions.AuthProtocolNotAvailableError as e:
-        logger.debug("%s - skipping callback endpoint" % e.message)
+        logger.debug("%s - skipping callback endpoint" % e)
     try:
         login.openid = openid.OpenIDController()
     except exceptions.AuthProtocolNotAvailableError as e:
-        logger.debug("%s - skipping callback endpoint" % e.message)
+        logger.debug("%s - skipping callback endpoint" % e)
     try:
         login.openid_connect = openid_connect.OpenIDConnectController()
     except exceptions.AuthProtocolNotAvailableError as e:
-        logger.debug("%s - skipping callback endpoint" % e.message)
+        logger.debug("%s - skipping callback endpoint" % e)
     try:
         login.SAML2 = SAML2.SAML2Controller()
     except exceptions.AuthProtocolNotAvailableError as e:
-        logger.debug("%s - skipping callback endpoint" % e.message)
+        logger.debug("%s - skipping callback endpoint" % e)
     about = introspection.IntrospectionController()
     apikey = apikey.APIKeyController()
 
